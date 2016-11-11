3bb048ac16d183a3fb0f816fd7db2d3901418ec3
emelyarellano1 <emelyac91@hotmail.com>
2016-11-11 14:27:45 -0300

    wapp

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function enviarTexto(){
+	var boton = getElementByid('enviar')
+	var contenedorTextos = document.classList('w-chat-panel')
+	var 
+	var contenedorListas = document.createElement('div');
+	var cajaText = document.createElement('h5');
+	var text =document.createTextNode("escribió");
+
+}
